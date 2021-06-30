2efb0182b6d1d10790edf124ab4f54110a8f5976
Szymon Wojdat <szymonwojdat13@gmail.com>
2021-06-30 18:25:14 +0200

    PokeTranslate 3.0 - rental image translation

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.executeScript({file: 'bundle.js'});
-});
+  chrome.tabs.executeScript({file: 'translate.js'});
+})
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    id: "poketranslate",
+    title: "To Pokepaste",
+    contexts: ["image"]
+  })
+})
+
+chrome.contextMenus.onClicked.addListener(function(clickData, tab) {
+  const imgURL = clickData["srcUrl"]
+  const url = `https://poketranslate.com/ocr/to_pokepaste/?img=${encodeURIComponent(imgURL)}`
+  window.open(url, '_blank').focus();
+})
